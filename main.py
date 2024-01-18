c243fb609b66f624166cb7da85f7b1b4874c8bbd
Tieraney <147672617+TieraneyRenee@users.noreply.github.com>
2024-01-18 00:38:11 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+# Week 1 Assignment | Tieraney Stewart
+# For this assignment we need to create a tutorial about a python feature
+# This code needs to have:
+#   Two Variables
+#   Input and Output 
+#   All the code comments
+
+# Print out information about the print command
+print('\n\nWelcome to Learning Python Because this is a Really Cool Tutorial!\n\n') # Whatever is put in the print '()' will be print out to the screen. It is a built in Python function. Single or double quotes can be used, but be consistent!
+#\n is the break for Python
+print ('In this lesson, we are going to master the "print" command in Python.\n\n') # Single quotation marks are good to use so that double can be used in the text that is printed out. A slash can also be used? There are different styles of doing things. Ultimately it is a personal preference.
+
+# Create two variables 
+# Gather user input
+userName = input('Hello! What is your name? (Type Your Name)  ') #
+readyYN = input('Are you ready? (Y/N)  ')
+
+# Display user input
+print('\n\nNice to meet you', userName)
+print('You answered with:', readyYN ) # Python is case sensitive! Copy and paste variable names to be sure that everything matches up. Remember tiny details are big! 
+
+# Print some more information about python
+print('\n\nWhat do you think the "print" command in Python is?')
+print('Okay! Time is up!')
+print('If you thought it was "print" then you would be correct!')
+print('------------')
+print('\n\nHere is the syntax for the "print" command in Python')
+print('print("Text to display on the screen.")')
+print('------------')
+print('\n\nHere is how you would use the "print" command in Python to print out the phrase "I am HAL 9000 Computer":')
+print('print("I am the HAL 9000 Computer")')
+print('\n\nThe following would display in your application')
+print('\nI am the HAL 9000 Computer')
+print('\n\nPro Tip: the print command must be all in lowercase.')
+
+# If an error happens the given line number may not be the exact line that the error is. But this is where to start looking.
+
+# Create the second variable
+rating = input('Please rate this tutorial on a scale of 1 to 1o. (with 10 being the absolute best) ')# Adding an extra space before the last closing ' will create a little space in what is printed.
+print('\nYou thought this tutorial was a: ',  rating) # With more advanced prgramming it would be possible to personalize the response that is printed depending on how the user answers.
+
+# Consistency is important however there may be times where things may need to be done a little differently. Be sure to leave comments in these instances!
+
+print('End of the tutorial. Have a good day!\n\n')
+
+# Main things taken from this assignment:
+#   Consistency always! If something has to be done different from how it has been done previously then be sure to leave a comment with why it had to be done. This will be improtant to prevent misunderstandings.
+#   As always with coding it seems like the little details are the ones that matter the most. Pyton is case sensitive! 'Print' won't do anything at all but 'print' will. 
